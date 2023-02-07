b561e52d3ac8b4ffde6cc2418bcd85826344d9da
User <nazarovtolya2@gmail.com>
2023-02-07 21:54:17 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int ageHuman = 14;
+
+        if (ageHuman < 18) {
+            System.out.println("Если возраст человека равен " + ageHuman + ", он не достиг совершеннолетия, нужно немного подождать.");
+        } else {
+            System.out.println("Ему " + ageHuman + " он достиг совершеннолетия"); }
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int temperature = 1;
+        if (temperature >= 5) {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        } else {
+            System.out.println("Сегодня холодно, нужно надеть шапку");
+        }
     }
 }
