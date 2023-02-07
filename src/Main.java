85a6ddffecff4c492c5080f40df2270160a0ff21
User <nazarovtolya2@gmail.com>
2023-02-07 22:05:10 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -17,11 +18,23 @@ public class Main {
 
     public static void task2() {
         System.out.println("Задача 2");
-        int temperature = 1;
+        int temperature = 5;
         if (temperature >= 5) {
             System.out.println("Сегодня тепло, можно идти без шапки");
         } else {
             System.out.println("Сегодня холодно, нужно надеть шапку");
         }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int speed = 59;
+
+        if (speed <= 60) {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно.");
+        } else {
+            System.out.println("Если скорость " + speed + ", придется заплатить штраф.");
+        }
+
     }
 }
